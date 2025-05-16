e35b534f6a41e15750ad67fec0f763abc0bf0d7b
IoannaTsenkova <ioannaradkova@gmail.com>
2025-05-16 18:46:38 +0300

    fix: allow mock service on production

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-import { Provider } from "@/components/ui/provider"
+import { Provider } from "@/components/ui/provider";
 
-if (import.meta.env.DEV) {
-  const { worker } = await import('./mocks/browser');
-  await worker.start();
-}
+const { worker } = await import("./mocks/browser");
+await worker.start();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider>
       <App />
